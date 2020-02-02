dd7e4943d3332b7dad1e5e830681367aaf3c4224
zarei-z <57044644+zarei-z@users.noreply.github.com>
2020-02-02 21:19:05 +0330

    add stored procedure & example of sp

---
+++ SQLQueryexampleofsp.sql
@@ -0,0 +1,11 @@
+EXEC SelectAllUsersbyname @fname = "homa"; /*find all user with first name =homa */
+
+exec selectmealsbyprice 9000 ; /*find all meals with price 9000*/
+
+
+/*exec Insertplacerecord   617 ,aghajoon , resturant ;*/
+
+
+/*EXEC stpUpdateuserByID   300 , '1356-12-10' ;*/
+
+/* EXEC deletedelivery   130 ; */
---
+++ SQLQuerysp.sql
@@ -0,0 +1,71 @@
+/* example of stored procedure */
+
+CREATE PROCEDURE SelectAllUsers
+AS
+SELECT * FROM users
+GO;
+
+CREATE PROCEDURE SelectAllUsersbyname @fname nvarchar(255)
+AS
+SELECT * FROM users WHERE fname = @fname
+
+
+
+create procedure selectmealsbyprice(
+ @mealprice money
+as
+select * from meal where mealprice = @mealprice;
+go; 
+
+
+Create Procedure Insertplacerecord  /*insert value to the place table */
+(
+ @placeid int,
+ @placename  Varchar(255),
+ @typep     Varchar(255)
+) 
+As
+ Begin
+   Insert into place (placeid, placename, typep)
+   Values(@placeid, @placename,@typep)
+ End
+ 
+
+
+ 
+
+   SET ANSI_NULLS ON  
+    GO  
+    SET QUOTED_IDENTIFIER ON  
+    GO  
+     
+    CREATE PROCEDURE stpUpdateuserByID  
+    @userid int,    
+    @birthdate date   
+    AS  
+    BEGIN    
+        SET NOCOUNT ON;  
+        UPDATE users  
+        Set   
+			birthdate=@birthdate
+        Where userid = @userid
+    END  
+    GO  
+	
+	
+    SET ANSI_NULLS ON  
+    GO  
+    SET QUOTED_IDENTIFIER ON  
+    GO  
+     
+    CREATE PROCEDURE deletedelivery  
+        @staffnum  int  
+    AS  
+    BEGIN   
+        SET NOCOUNT ON;  
+      
+        Delete from delivery
+        where staffnum = @staffnum
+          
+    END  
+    GO  
