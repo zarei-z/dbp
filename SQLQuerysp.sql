26f64343dafb7559a188d67a0f0b4e5feb1b38e1
zarei-z <57044644+zarei-z@users.noreply.github.com>
2020-02-04 09:26:50 +0330

    add two sp

--- SQLQuerysp.sql
+++ SQLQuerysp.sql
@@ -46,7 +46,41 @@ As
  End
  go
 	
+Create Procedure UserAdd  /*insert value to the place table */
+(
+ @userid int,
+ @fname  Varchar(255),
+ @lname    Varchar(255)
+) 
+As
+ Begin
+   Insert into users(userid, fname, lname)
+   Values(@userid, @fname,@lname)
+ End
+ go
 	
+Create Procedure OrderAdd  /*insert value to the order table */
+(
+ @sttafnum  int ,
+ @userid	int,
+ @placeid	int ,
+ @del_pay	money ,
+ @place_pay	money ,
+ @phone1	char(10),
+ @address1	nvarchar(50),
+ @city1		varchar(50),
+ @phone2	char(10),
+ @address2	nvarchar(50),
+ @city2		varchar(50),
+ @mealid	int
+) 
+As
+ Begin
+   Insert into orders(staffnum ,userid, placeid ,del_pay , place_pay , phone1 , address1 , city1 , phone2 , address2 ,city2 , mealid)
+   Values(@sttafnum , @userid ,@placeid , @del_pay , @place_pay , @phone1 , @address1 , @city1 , @phone2 ,@address2 , @city2 , @mealid)
+ End
+ go 
+
     CREATE PROCEDURE stpUpdateuserByID(  /*update the birthdate of user with user id */
 
     @userid int,    
